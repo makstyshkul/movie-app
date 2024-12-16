e49e4a81efb76b96c3b50c26060a3c135801f26f
makstshkl <maxt2004@ukr.net>
2024-12-16 11:55:13 +0200

    add pipes and directives for project

--- angular-course/src/app/app.component.ts
+++ angular-course/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import {Component} from '@angular/core';
 import { MovieListComponent } from './project/components/movie-list/movie-list.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ MovieListComponent],
+  imports: [ MovieListComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
