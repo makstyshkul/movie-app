7dc39dbf034e1178c1b268d4df3a10b155d7edf1
makstshkl <maxt2004@ukr.net>
2025-07-16 20:02:18 +0300

    Restructure: moved Angular project to root

---
+++ 17.3.8/vite/deps/primeng_button.js
@@ -0,0 +1,19 @@
+import {
+  Button,
+  ButtonDirective,
+  ButtonModule
+} from "./chunk-5RBSAZOV.js";
+import "./chunk-TMBZJZRI.js";
+import "./chunk-LC2U7RES.js";
+import "./chunk-NNVF23JD.js";
+import "./chunk-4UTMUNVM.js";
+import "./chunk-7XYLTVQC.js";
+import "./chunk-LMFWYZLC.js";
+import "./chunk-KT3RQDJZ.js";
+import "./chunk-RVQ3RH6Y.js";
+export {
+  Button,
+  ButtonDirective,
+  ButtonModule
+};
+//# sourceMappingURL=primeng_button.js.map
